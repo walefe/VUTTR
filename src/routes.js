99cffcd94d7e5f7bbd55db0a6aec223bf482b087
Walefe <walefedev@gmail.com>
2019-08-30 14:56:42 -0300

    Chore: Add session controller.

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = Router();
 
 routes.post('/users', UserController.store);
+
+routes.post('/sessions', SessionController.store);
 
 export default routes;
