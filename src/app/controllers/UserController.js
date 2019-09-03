bd882575f480e5e736b2af17b25c4dcc63dfa426
Walefe <walefedev@gmail.com>
2019-09-03 11:06:37 -0300

    Chore: add data validation on controllers.

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,18 @@
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails.' });
+    }
     const userExists = await User.findOne({ where: { email: req.body.email } });
 
     if (userExists) {
